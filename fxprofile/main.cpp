ad26c930b7bb8e512f8c37954d636813dfac39a1
724789975 <724789975@qq.com>
2023-03-16 21:28:47 +0800

    windows获取堆栈
    Signed-off-by: 724789975 <724789975@qq.com>

--- fxprofile/main.cpp
+++ fxprofile/main.cpp
@@ -1,4 +1,5 @@
 #ifdef _WIN32
+#include <WinSock2.h>
 #include <windows.h>
 
 #include <Mmsystem.h>
@@ -94,7 +95,7 @@ void OnSig45(int n, siginfo_t* sinfo, void* p)
 #endif // _WIN32
 }
 
-int main()
+int main1()
 {
 #ifdef _WIN32
 	signal(SIGINT, OnSig45);
@@ -133,3 +134,167 @@ int main()
 	}
 	return 0;
 }
+
+
+#include <Windows.h>
+#include <processthreadsapi.h>
+#include <Tlhelp32.h>
+#include <DbgHelp.h>
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <tuple>
+#include <thread>
+#include<assert.h>
+#include <list>
+
+#pragma comment(lib, "dbghelp.lib")
+
+HANDLE proc;
+struct TestThreadContext {
+	static void workert() {
+		for (int j = 0; j < 100000; ++j) {
+			if (j % 100 == 0);
+				//std::cout << "thread " << GetCurrentThreadId() << " run" << std::endl;
+			Sleep(100);
+		}
+	}
+	// 获取其它线程堆栈
+	// stacks {线程id， 调用栈，错误说明}
+	static void get_threads_stack(std::list < std::tuple<unsigned, std::string, std::string >>& stacks) {
+		auto pid = GetCurrentProcessId();
+		auto tid = GetCurrentThreadId();
+		//HANDLE proc = GetCurrentProcess();
+		//SymInitialize(proc, NULL, TRUE);
+		HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, pid);
+		if (snap == INVALID_HANDLE_VALUE) {
+			return;
+		}
+		THREADENTRY32 e = { sizeof(e) };
+		BOOL ok = Thread32First(snap, &e);
+		for (; ok; ok = Thread32Next(snap, &e)) {
+			if (e.th32OwnerProcessID != pid || e.th32ThreadID == tid)
+				continue;
+			DWORD thread_id = e.th32ThreadID;
+			//cout << "open thread " << e.th32ThreadID << endl;
+			HANDLE th = OpenThread(THREAD_SUSPEND_RESUME | THREAD_GET_CONTEXT, FALSE, thread_id);
+			if (th == INVALID_HANDLE_VALUE) {
+				//cout << "open thread " << tid << " failed" << endl;
+				stacks.emplace_back(thread_id, "", "error: open failed");
+				continue;
+			}
+			DWORD ret = SuspendThread(th);
+			if (ret == (DWORD)-1) {
+				CloseHandle(th);
+				stacks.emplace_back(thread_id, "", "error: SuspendThread failed");
+				continue;
+			}
+			CONTEXT ctx;
+			ZeroMemory(&ctx, sizeof(ctx));
+			ctx.ContextFlags = CONTEXT_ALL;
+			ret = GetThreadContext(th, &ctx);
+			if (!ret) {
+				ResumeThread(th);
+				CloseHandle(th);
+				stacks.emplace_back(thread_id, "", "error: GetThreadContext failed");
+				continue;
+			}
+			//
+			STACKFRAME sf = { 0 };
+			sf.AddrPC.Offset = ctx.Rip;
+			sf.AddrPC.Mode = AddrModeFlat;
+			sf.AddrFrame.Offset = ctx.Rbp;
+			sf.AddrFrame.Mode = AddrModeFlat;
+			sf.AddrStack.Offset = ctx.Rsp;
+			sf.AddrStack.Mode = AddrModeFlat;
+			//
+			typedef struct tag_SYMBOL_INFO
+			{
+				IMAGEHLP_SYMBOL symInfo;
+				TCHAR szBuffer[MAX_PATH];
+			} SYMBOL_INFO, * LPSYMBOL_INFO;
+			// 32位系统下变量类型是DWORD ，64位系统下则是DWORD64
+			decltype(sf.AddrPC.Offset) dwDisplament = 0;
+			DWORD dwDis32 = 0;
+			SYMBOL_INFO stack_info = { 0 };
+			PIMAGEHLP_SYMBOL pSym = (PIMAGEHLP_SYMBOL)&stack_info;
+			pSym->SizeOfStruct = sizeof(IMAGEHLP_SYMBOL);
+			pSym->MaxNameLength = sizeof(SYMBOL_INFO) - offsetof(SYMBOL_INFO, symInfo.Name);
+			IMAGEHLP_LINE ImageLine = { 0 };
+			ImageLine.SizeOfStruct = sizeof(IMAGEHLP_LINE);
+			std::string stack = "";
+			while (StackWalk(IMAGE_FILE_MACHINE_AMD64, proc, th, &sf, &ctx,
+				NULL, SymFunctionTableAccess, SymGetModuleBase, NULL))
+			{
+				char buf[512];
+				if (SymGetSymFromAddr(proc, sf.AddrPC.Offset, &dwDisplament, pSym)) {
+					if (SymGetLineFromAddr(proc, sf.AddrPC.Offset, &dwDis32, &ImageLine)) {
+						char* fullpath = ImageLine.FileName;
+						// find file name in full path
+						char* f = fullpath + strlen(fullpath);
+						while (*f != '\\' && f > fullpath) --f;
+						if (f > fullpath)
+							++f;
+						snprintf(buf, sizeof(buf), "%#llx+%s [%s: %d]\n",
+							pSym->Address, pSym->Name, f, ImageLine.LineNumber);
+					}
+					else {
+						snprintf(buf, sizeof(buf), "%#llx+%s\n", pSym->Address, pSym->Name);
+					}
+				}
+				else {
+					snprintf(buf, sizeof(buf), "%#llx, err: %d\n", sf.AddrPC.Offset, GetLastError());
+				}
+				stack.append(buf);
+			}
+			stacks.emplace_back(thread_id, stack, "");
+			ResumeThread(th);
+			CloseHandle(th);
+		}
+		CloseHandle(snap);
+		//SymCleanup(proc);
+	}
+
+	// 测试获取其它线程堆栈并打印
+	static void master() {
+		const int num = 4;
+		std::thread ws[num];
+		for (int j = 0; j < num; ++j)
+			ws[j] = std::thread(workert);
+		for (int j = 0; j < num; ++j)
+			ws[j].detach();
+		Sleep(5000);
+		auto pid = GetCurrentProcessId();
+		auto tid = GetCurrentThreadId();
+		proc = GetCurrentProcess();
+		SymInitialize(proc, NULL, TRUE);
+		for (;;)
+		{
+			std::list < std::tuple<unsigned, std::string, std::string >> stacks;
+			get_threads_stack(stacks);
+			for (auto& t : stacks) {
+				unsigned tid;
+				std::string stack, error;
+				std::tie(tid, stack, error) = t;
+				if (error.empty()) {
+					std::cout << "thread " << tid << "\n" << stack << std::endl;
+				}
+				else {
+					std::cout << "thread " << tid << " " << error << std::endl;
+				}
+			}
+
+			Sleep(5000);
+		}
+	}
+};
+
+int main2(int argc, char* argv[])
+{
+	TestThreadContext::master();
+	return 0;
+}
+
+
+
+
